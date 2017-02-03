eec5f504ddfd254aeb0c647584e1d7aad9f507ba
Steve Tripp <17165194+stevetripp@users.noreply.github.com>
2017-02-03 03:09:53 +0000

    Add missing file.

---
+++ Libraries/TNT.LSD.Plugins/DGVPartsEditorPlugin.cs
@@ -0,0 +1,34 @@
+﻿using LSDComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TNT.LSD.Inventory;
+using TNT.LSD.Plugins.Support;
+
+namespace TNT.LSD.Plugins
+{
+	public class DGVPartsEditorPlugin : Plugin.Plugin
+	{
+		protected DGVPartsEditor _Editor = new DGVPartsEditor();
+
+		public override string Text { get { return this._Editor.Text; } }
+
+		public override string ToolTipText { get { return "Edit the static parts listing"; } }
+
+		public override System.Drawing.Image Image { get { return this._Editor.Icon.ToBitmap(); } }
+
+		protected override object Execute(System.Windows.Forms.IWin32Window owner, object parameter)
+		{
+			TNTCAD cad = parameter as TNTCAD;
+
+			if (cad != null)
+			{
+				new DGVPartsEditor().ShowDialog(owner, cad.State.StaticParts);
+			}
+
+			return null;
+		}
+	}
+}
