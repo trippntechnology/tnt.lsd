2bd5e3cdb195151c6f977e6c10355ac59d72c4af
Steve Tripp <17165194+stevetripp@users.noreply.github.com>
2018-04-18 02:48:00 +0000

    Add missing files.

---
+++ Plugins/TNT.LSD.Checker/OutputForm.cs
@@ -0,0 +1,46 @@
+﻿using LSDComponents;
+using System;
+using System.Drawing;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace TNT.LSD.Checker
+{
+	public partial class OutputForm : DockContent
+	{
+		public OutputForm()
+		{
+			InitializeComponent();
+		}
+
+		internal void Show(ApplicationData applicationData, DockState document)
+		{
+			Show(applicationData.DockPanel, document);
+
+			appendText("Blue", Color.Blue);
+			appendText("Yellow", Color.Yellow);
+			appendLine("Default");
+			appendLine("Red", Color.Red);
+			appendLine("Default bold", fontStyle: FontStyle.Bold);
+
+		}
+
+		private void appendText(string text, Color? color = null, FontStyle fontStyle = FontStyle.Regular)
+		{
+			Color selectionColor = color ?? console.ForeColor;
+
+			console.SelectionStart = console.TextLength;
+			console.SelectionLength = 0;
+
+			console.SelectionColor = selectionColor;
+			console.SelectionFont = new Font(console.Font, fontStyle);
+			console.AppendText(text);
+			console.SelectionColor = console.ForeColor;
+		}
+
+		private void appendLine(string text, Color? color = null, FontStyle fontStyle = FontStyle.Regular)
+		{
+			appendText(text, color, fontStyle);
+			appendText(Environment.NewLine);
+		}
+	}
+}
