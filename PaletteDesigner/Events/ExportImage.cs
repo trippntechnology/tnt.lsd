b6ae767fc2054b4fbcde8a18c76100d14760fd66
Steve <stevetripp@comcast.net>
2018-12-21 19:01:10 -0700

    Add ability to export the node's image.

---
+++ PaletteDesigner/Events/ExportImage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PalletDesigner.Events
+{
+	class ExportImage : NodeEvents
+	{
+		public override string Text => "Export Image";
+
+		public override string ToolTipText => "Export image associated with node";
+
+		public override void MouseClick(object sender, EventArgs e)
+		{
+			base.MouseClick(sender, e);
+			using (var sfd = new SaveFileDialog())
+			{
+				if (sfd.ShowDialog() == DialogResult.OK)
+				{
+					PalletNodeTreeView.ExportImage(sfd.FileName);
+				}
+			}
+		}
+	}
+}
