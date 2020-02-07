44aa4fd86e9e4ca39796bbc045f37229fdacfe08
Steve <stevetripp@comcast.net>
2020-02-06 21:04:46 -0700

    Add coverage page to pdf.
    Create copy of TNTCAD so that changes to get the images don't effect the layout.

--- Plugins/TNT.LSD.SSC/Plugin.cs
+++ Plugins/TNT.LSD.SSC/Plugin.cs
@@ -14,14 +14,15 @@ namespace TNT.LSD.SSC
 
 		public override bool LicenseRequired => true;
 
-		protected void GeneratePDF(ApplicationData appData, TNTCAD cad, string fileName)
+		protected void GeneratePDF(ApplicationData appData, TNTCAD _cad, string fileName)
 		{
+			// Create a copy so that it doesn't effect the actual layout
+			var cad = new TNTCAD(_cad);
+
 			// Unselect all objects so that they are included in the drawn image for the PDF
 			cad.UnselectAll();
 			cad.Repaint();
 
-			var scale = cad.DisplayScale;
-			var showCoverage = cad.DrawingOptions.ShowCoverage;
 			cad.DisplayScale = 100;
 			cad.DrawingOptions.ShowCoverage = false;
 
@@ -51,9 +52,6 @@ namespace TNT.LSD.SSC
 			}
 
 			(new PDFGenerator.SSCPDFGenerator()).Generate(fileName, pdfContent);
-
-			cad.DisplayScale = scale;
-			cad.DrawingOptions.ShowCoverage = showCoverage;
 
 			PDFForm pdfForm = new PDFForm();
 			pdfForm.Show(fileName, appData.DockPanel, DockState.Document);
