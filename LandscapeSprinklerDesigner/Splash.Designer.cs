8919008597d9b3f012f7985d966f1207fa5ae862
Steve <stevetripp@comcast.net>
2020-02-11 19:49:14 -0700

    Update splash

--- LandscapeSprinklerDesigner/Splash.Designer.cs
+++ LandscapeSprinklerDesigner/Splash.Designer.cs
@@ -29,19 +29,32 @@
 		private void InitializeComponent()
 		{
 			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Splash));
 			this.timer = new System.Windows.Forms.Timer(this.components);
+			this.versionPlaceholder = new System.Windows.Forms.Panel();
 			this.SuspendLayout();
 			// 
 			// timer
 			// 
-			this.timer.Interval = 2000;
+			this.timer.Interval = 5000;
 			this.timer.Tick += new System.EventHandler(this.timer_Tick);
+			// 
+			// panelVersion
+			// 
+			this.versionPlaceholder.BackColor = System.Drawing.Color.Transparent;
+			this.versionPlaceholder.Location = new System.Drawing.Point(414, 161);
+			this.versionPlaceholder.Name = "panelVersion";
+			this.versionPlaceholder.Size = new System.Drawing.Size(200, 100);
+			this.versionPlaceholder.TabIndex = 0;
 			// 
 			// Splash
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 32F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(155, 145);
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(709, 558);
+			this.Controls.Add(this.versionPlaceholder);
 			this.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(246)))), ((int)(((byte)(255)))));
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -51,6 +64,7 @@
 			this.Text = "Splash";
 			this.Load += new System.EventHandler(this.Splash_Load);
 			this.Paint += new System.Windows.Forms.PaintEventHandler(this.Splash_Paint);
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Splash_MouseDown);
 			this.ResumeLayout(false);
 
 		}
@@ -58,5 +72,6 @@
 		#endregion
 
 		private System.Windows.Forms.Timer timer;
+		private System.Windows.Forms.Panel versionPlaceholder;
 	}
 }
