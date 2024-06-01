7cad03955cdf922ba29f0a5dd21de476b6c42937
Steve Tripp <steve@trippntechnology.com>
2024-06-01 17:24:18 -0600

    Update TNT.LSD.Checker to .Net8

--- Plugins/TNT.LSD.Checker/OutputForm.Designer.cs
+++ Plugins/TNT.LSD.Checker/OutputForm.Designer.cs
@@ -1,64 +1,63 @@
-﻿namespace TNT.LSD.Checker
+﻿namespace TNT.LSD.Checker;
+
+partial class OutputForm
 {
-	partial class OutputForm
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+  /// <summary>
+  /// Required designer variable.
+  /// </summary>
+  private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+  /// <summary>
+  /// Clean up any resources being used.
+  /// </summary>
+  /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+  protected override void Dispose(bool disposing)
+  {
+    if (disposing && (components != null))
+    {
+      components.Dispose();
+    }
+    base.Dispose(disposing);
+  }
 
-		#region Windows Form Designer generated code
+  #region Windows Form Designer generated code
 
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OutputForm));
-			this.console = new System.Windows.Forms.RichTextBox();
-			this.SuspendLayout();
-			// 
-			// console
-			// 
-			this.console.BackColor = System.Drawing.SystemColors.WindowText;
-			this.console.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.console.ForeColor = System.Drawing.SystemColors.Window;
-			this.console.Location = new System.Drawing.Point(0, 0);
-			this.console.Name = "console";
-			this.console.ReadOnly = true;
-			this.console.Size = new System.Drawing.Size(800, 450);
-			this.console.TabIndex = 0;
-			this.console.Text = "";
-			// 
-			// OutputForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.console);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "OutputForm";
-			this.Text = "Design Check";
-			this.ResumeLayout(false);
+  /// <summary>
+  /// Required method for Designer support - do not modify
+  /// the contents of this method with the code editor.
+  /// </summary>
+  private void InitializeComponent()
+  {
+    System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OutputForm));
+    this.console = new System.Windows.Forms.RichTextBox();
+    this.SuspendLayout();
+    // 
+    // console
+    // 
+    this.console.BackColor = System.Drawing.SystemColors.WindowText;
+    this.console.Dock = System.Windows.Forms.DockStyle.Fill;
+    this.console.ForeColor = System.Drawing.SystemColors.Window;
+    this.console.Location = new System.Drawing.Point(0, 0);
+    this.console.Name = "console";
+    this.console.ReadOnly = true;
+    this.console.Size = new System.Drawing.Size(800, 450);
+    this.console.TabIndex = 0;
+    this.console.Text = "";
+    // 
+    // OutputForm
+    // 
+    this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    this.ClientSize = new System.Drawing.Size(800, 450);
+    this.Controls.Add(this.console);
+    this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+    this.Name = "OutputForm";
+    this.Text = "Design Check";
+    this.ResumeLayout(false);
 
-		}
+  }
 
-		#endregion
+  #endregion
 
-		private System.Windows.Forms.RichTextBox console;
-	}
+  private System.Windows.Forms.RichTextBox console;
 }
--- Plugins/TNT.LSD.Checker/Plugin.cs
+++ Plugins/TNT.LSD.Checker/Plugin.cs
@@ -1,46 +1,45 @@
-﻿using LSDComponents;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using TNT.LSD.Components;
 using TNT.Plugin.Manager;
 using WeifenLuo.WinFormsUI.Docking;
 
-namespace TNT.LSD.Checker
+namespace TNT.LSD.Checker;
+
+class Plugin : TNT.Plugin.Manager.Plugin
 {
-	class Plugin : TNT.Plugin.Manager.Plugin
-	{
-		private const string imageName = "TNT.LSD.Checker.Images.eye.png";
+  private const string imageName = "TNT.LSD.Checker.Images.eye.png";
 
-		public override string MenuStripName => "MenuStrip1";
+  public override string MenuStripName => "MenuStrip1";
 
-		public override string ToolStripName => "ToolStrip3";
+  public override string ToolStripName => "ToolStrip3";
 
-		public override string Text => "Check Design";
+  public override string Text => "Check Design";
 
-		public override string ToolTipText => "Check design for issues";
+  public override string ToolTipText => "Check design for issues";
 
-		public override System.Drawing.Image Image => base.GetImage(imageName);
+  public override System.Drawing.Image Image => base.GetImage(imageName);
 
-		public override void Execute(System.Windows.Forms.IWin32Window owner, System.Windows.Forms.ToolStripItem sender, IApplicationData content)
-		{
-			var applicationData = content as ApplicationData;
+  public override void Execute(System.Windows.Forms.IWin32Window owner, System.Windows.Forms.ToolStripItem sender, IApplicationData content)
+  {
+    var applicationData = content as ApplicationData;
 
-			var outputForm = new OutputForm();
-			outputForm.Show(applicationData, DockState.Document);
-		}
+    var outputForm = new OutputForm();
+    outputForm.Show(applicationData, DockState.Document);
+  }
 
-		public override System.Windows.Forms.MenuStrip GetMenuStrip()
-		{
-			return null;
-		}
+  public override System.Windows.Forms.MenuStrip GetMenuStrip()
+  {
+    return null;
+  }
 
-		public override System.Windows.Forms.ToolStrip GetToolStrip()
-		{
-			ToolStrip toolStrip = new ToolStrip();
+  public override System.Windows.Forms.ToolStrip GetToolStrip()
+  {
+    ToolStrip toolStrip = new ToolStrip();
 
-			ToolStripButton toolStripButton = (ToolStripButton)CreateToolStripItem<ToolStripButton>(this.Text, base.GetImage(imageName), this.ToolTipText);
-			toolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
-			toolStrip.Items.Add(toolStripButton);
+    ToolStripButton toolStripButton = (ToolStripButton)CreateToolStripItem<ToolStripButton>(this.Text, base.GetImage(imageName), this.ToolTipText);
+    toolStripButton.DisplayStyle = ToolStripItemDisplayStyle.Image;
+    toolStrip.Items.Add(toolStripButton);
 
-			return toolStrip;
-		}
-	}
+    return toolStrip;
+  }
 }
