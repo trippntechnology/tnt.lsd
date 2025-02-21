396bafd5009b0d55f3b26ed884c4f463481d678b
Steve Tripp <steve@trippntechnology.com>
2025-02-21 09:29:22 -0700

    Add DripAdapter and PointExtension test.

--- Libraries/TNT.LSD.Objects/Parts/DripAdapter.cs
+++ Libraries/TNT.LSD.Objects/Parts/DripAdapter.cs
@@ -1,135 +1,115 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using TNT.LSD.Inventory;
 using TNT.LSD.Settings;
 
-namespace TNT.LSD.Objects
+namespace TNT.LSD.Objects;
+
+/// <summary>
+/// Stub type
+/// </summary>
+public enum StubType { PVC, FunnyPipe }
+
+/// <summary>
+/// Represents a drip adapter
+/// </summary>
+public class DripAdapter : LateralPart
 {
-	/// <summary>
-	/// Stub type
-	/// </summary>
-	public enum StubType { PVC, FunnyPipe }
+  /// <summary>
+  /// Type of pipe stubbed to the surface
+  /// </summary>
+  [Description("Type of pipe stubbed to the surface")]
+  [ReadOnly(true)]
+  public StubType Stub { get; set; }
 
-	/// <summary>
-	/// Represents a drip adapter
-	/// </summary>
-	public class DripAdapter : LateralPart
-	{
-		/// <summary>
-		/// Type of pipe stubbed to the surface
-		/// </summary>
-		[Description("Type of pipe stubbed to the surface")]
-		[ReadOnly(true)]
-		public StubType Stub { get; set; }
+  #region Constructors
 
-		#region Constructors
+  /// <summary>
+  /// Copy constructor
+  /// </summary>
+  /// <param name="obj">Object to copy</param>
+  public DripAdapter(DripAdapter obj)
+    : base(obj)
+  {
+    Stub = obj.Stub;
+  }
 
-		/// <summary>
-		/// Copy constructor
-		/// </summary>
-		/// <param name="obj">Object to copy</param>
-		public DripAdapter(DripAdapter obj)
-			: base(obj)
-		{
-			Stub = obj.Stub;
-		}
+  /// <summary>
+  /// Default constructor
+  /// </summary>
+  public DripAdapter()
+    : base()
+  {
+  }
 
-		/// <summary>
-		/// Default constructor
-		/// </summary>
-		public DripAdapter()
-			: base()
-		{
-		}
+  #endregion
 
-		#endregion
+  /// <summary>
+  /// Clones this <see cref="DripAdapter"/>
+  /// </summary>
+  /// <returns></returns>
+  public override TNTObject Clone() => new DripAdapter(this);
 
-		/// <summary>
-		/// Clones this <see cref="DripAdapter"/>
-		/// </summary>
-		/// <returns></returns>
-		public override TNTObject Clone() => new DripAdapter(this);
+  /// <summary>
+  /// Adds the parts needed to stub off the pipe
+  /// </summary>
+  [ExcludeFromCodeCoverage]
+  public override void SetPartQuantity(CodedParts parts, SystemType systemType)
+  {
+    base.SetPartQuantity(parts, systemType);
 
-		/// <summary>
-		/// Adds the parts needed to stub off the pipe
-		/// </summary>
-		public override void SetPartQuantity(CodedParts parts, SystemType systemType)
-		{
-			base.SetPartQuantity(parts, systemType);
+    var maxPipeSize = GetMaxPipeSize(typeof(LateralPipe));
+    var minPipeSize = Pipes.Count > 1 ? GetMinPipeSize(typeof(LateralPipe)) : null;
 
-			var maxPipeSize = GetMaxPipeSize(typeof(LateralPipe));
-			var minPipeSize = Pipes.Count > 1 ? GetMinPipeSize(typeof(LateralPipe)) : null;
-
-			// Only add parts if there is a lateral line connected
+    // Only add parts if there is a lateral line connected
 			if (maxPipeSize == null) return;
 
-			if (systemType == SystemType.PVC)
-			{
-				AddPVCFittings(parts, maxPipeSize, minPipeSize);
-			}
-			else
-			{
-				AddPOLYFittings(parts, maxPipeSize, minPipeSize);
-			}
+    if (systemType == SystemType.PVC)
+    {
+      AddPVCFittings(parts, maxPipeSize, minPipeSize);
+    }
+    else
+    {
+      AddPOLYFittings(parts, maxPipeSize, minPipeSize);
+    }
+  }
 
-			//if (Stub == StubType.PVC)
-			//{
-			//	if (minPipeSize == null)
-			//	{
-			//		parts.Add($"FI{maxPipeSize.Code}SS90", 1);
-			//	}
-			//	else
-			//	{
-			//		parts.Add($"FI{maxPipeSize.Code}SSSTEE", 1);
+  /// <summary>
+  /// Add PVC fittings
+  /// </summary>
+  public static void AddPVCFittings(CodedParts parts, PartSize maxPipeSize, PartSize minPipeSize)
+  {
+    if (minPipeSize == null)// && maxPipeSize <= PartSize.SIZE_100)
+    {
+      parts.Add($"FI{maxPipeSize.Code}X{PartSize.SIZE_050}ST90", 1);
+    }
+    else
+    {
+      parts.Add($"FI{maxPipeSize.Code}X{PartSize.SIZE_050}SSTTEE", 1);
+    }
 
-			//	}
+    parts.Add("FPSBE050", 1);
+    parts.Add("FUNNYPIPE", 1);
+    parts.Add("HRFIG8", 1);
+  }
 
-			//	if (maxPipeSize > PartSize.SIZE_075)
-			//	{
-			//		parts.Add($"FI{maxPipeSize.Code}X{PartSize.SIZE_075.Code}SSRB", 1);
-			//	}
+  /// <summary>
+  /// Add POLY fittings
+  /// </summary>
+  public static void AddPOLYFittings(CodedParts parts, PartSize maxPipeSize, PartSize minPipeSize)
+  {
+    if (minPipeSize == null)
+    {
+      parts.Add($"PF{maxPipeSize}X{PartSize.SIZE_050}BT90", 1);
+      parts.AddHoseClamp(maxPipeSize.Code, 1);
+    }
+    else
+    {
+      parts.Add($"PF{maxPipeSize.Code}HDSADDLE", 1);
+    }
 
-			//	parts.Add($"PI{PartSize.SIZE_075.Code}", 1);
-			//	parts.Add($"FI{PartSize.SIZE_075.Code}SCAP", 1);
-			//}
-		}
-
-		/// <summary>
-		/// Add PVC fittings
-		/// </summary>
-		public static void AddPVCFittings(CodedParts parts, PartSize maxPipeSize, PartSize minPipeSize)
-		{
-			if (minPipeSize == null)// && maxPipeSize <= PartSize.SIZE_100)
-			{
-				parts.Add($"FI{maxPipeSize.Code}X{PartSize.SIZE_050}ST90", 1);
-			}
-			else
-			{
-				parts.Add($"FI{maxPipeSize.Code}X{PartSize.SIZE_050}SSTTEE", 1);
-			}
-
-			parts.Add("FPSBE050", 1);
-			parts.Add("FUNNYPIPE", 1);
-			parts.Add("HRFIG8", 1);
-		}
-
-		/// <summary>
-		/// Add POLY fittings
-		/// </summary>
-		public static void AddPOLYFittings(CodedParts parts, PartSize maxPipeSize, PartSize minPipeSize)
-		{
-			if (minPipeSize == null)
-			{
-				parts.Add($"PF{maxPipeSize}X{PartSize.SIZE_050}BT90", 1);
-				parts.AddHoseClamp(maxPipeSize.Code, 1);
-			}
-			else
-			{
-				parts.Add($"PF{maxPipeSize.Code}HDSADDLE", 1);
-			}
-
-			parts.Add("FPSBE050", 1);
-			parts.Add("FUNNYPIPE", 1);
-			parts.Add("HRFIG8", 1);
-		}
-	}
+    parts.Add("FPSBE050", 1);
+    parts.Add("FUNNYPIPE", 1);
+    parts.Add("HRFIG8", 1);
+  }
 }
--- Tests/TNT.LSD.Objects.Tests/DripAdapterTests.cs
+++ Tests/TNT.LSD.Objects.Tests/DripAdapterTests.cs
@@ -2,88 +2,104 @@
 using TNT.LSD.Inventory;
 using TNT.LSD.Objects;
 
-namespace TNTObjectsTests
+namespace TNTObjectsTests;
+
+[ExcludeFromCodeCoverage]
+public class DripAdapterTests
 {
-  [ExcludeFromCodeCoverage]
-  public class DripAdapterTests
+  [Test]
+  public void AddPVCFittings_Single_Pipe()
   {
-    [Test]
-    public void AddPVCFittings_Single_Pipe()
+    var parts = new CodedParts();
+    var sizes = new List<PartSize>() { PartSize.SIZE_075, PartSize.SIZE_100, PartSize.SIZE_125 };
+
+    foreach (var pipeSize in sizes)
     {
-      var parts = new CodedParts();
-      var sizes = new List<PartSize>() { PartSize.SIZE_075, PartSize.SIZE_100, PartSize.SIZE_125 };
+      parts.Clear();
+      DripAdapter.AddPVCFittings(parts, pipeSize, null);
 
-      foreach (var pipeSize in sizes)
-      {
-        parts.Clear();
-        DripAdapter.AddPVCFittings(parts, pipeSize, null);
-
-        Assert.That(parts.Count, Is.EqualTo(4));
-        Assert.That(parts[$"FI{pipeSize}X050ST90"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"FPSBE050"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"FUNNYPIPE"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"HRFIG8"].Quantity, Is.EqualTo(1));
-      }
+      Assert.That(parts.Count, Is.EqualTo(4));
+      Assert.That(parts[$"FI{pipeSize}X050ST90"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"FPSBE050"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"FUNNYPIPE"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"HRFIG8"].Quantity, Is.EqualTo(1));
     }
+  }
 
-    [Test]
-    public void AddPVCFittings_Double_Pipe()
+  [Test]
+  public void AddPVCFittings_Double_Pipe()
+  {
+    var parts = new CodedParts();
+    var sizes = new List<PartSize>() { PartSize.SIZE_075, PartSize.SIZE_100, PartSize.SIZE_125, PartSize.SIZE_150, PartSize.SIZE_200 };
+
+    foreach (var pipeSize in sizes)
     {
-      var parts = new CodedParts();
-      var sizes = new List<PartSize>() { PartSize.SIZE_075, PartSize.SIZE_100, PartSize.SIZE_125, PartSize.SIZE_150, PartSize.SIZE_200 };
+      parts.Clear();
+      DripAdapter.AddPVCFittings(parts, pipeSize, pipeSize);
 
-      foreach (var pipeSize in sizes)
-      {
-        parts.Clear();
-        DripAdapter.AddPVCFittings(parts, pipeSize, pipeSize);
-
-        Assert.That(parts.Count, Is.EqualTo(4));
-        Assert.That(parts[$"FI{pipeSize}X050SSTTEE"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"FPSBE050"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"FUNNYPIPE"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"HRFIG8"].Quantity, Is.EqualTo(1));
-      }
+      Assert.That(parts.Count, Is.EqualTo(4));
+      Assert.That(parts[$"FI{pipeSize}X050SSTTEE"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"FPSBE050"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"FUNNYPIPE"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"HRFIG8"].Quantity, Is.EqualTo(1));
     }
+  }
 
-    [Test]
-    public void AddPOLYFittings_Single_Pipe()
+  [Test]
+  public void AddPOLYFittings_Single_Pipe()
+  {
+    var parts = new CodedParts();
+    var sizes = new List<PartSize>() { PartSize.SIZE_075, PartSize.SIZE_100, PartSize.SIZE_125 };
+
+    foreach (var pipeSize in sizes)
     {
-      var parts = new CodedParts();
-      var sizes = new List<PartSize>() { PartSize.SIZE_075, PartSize.SIZE_100, PartSize.SIZE_125 };
+      var hc = pipeSize == PartSize.SIZE_125 ? PartSize.SIZE_150 : pipeSize;
 
-      foreach (var pipeSize in sizes)
-      {
-        var hc = pipeSize == PartSize.SIZE_125 ? PartSize.SIZE_150 : pipeSize;
+      parts.Clear();
+      DripAdapter.AddPOLYFittings(parts, pipeSize, null);
 
-        parts.Clear();
-        DripAdapter.AddPOLYFittings(parts, pipeSize, null);
-
-        Assert.That(parts.Count, Is.EqualTo(5));
-        Assert.That(parts[$"PF{pipeSize}X050BT90"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"FPSBE050"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"FUNNYPIPE"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"HRFIG8"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"HC{hc}"].Quantity, Is.EqualTo(1));
-      }
+      Assert.That(parts.Count, Is.EqualTo(5));
+      Assert.That(parts[$"PF{pipeSize}X050BT90"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"FPSBE050"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"FUNNYPIPE"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"HRFIG8"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"HC{hc}"].Quantity, Is.EqualTo(1));
     }
+  }
 
-    [Test]
-    public void AddPOLYFittings_Double_Pipe()
+  [Test]
+  public void AddPOLYFittings_Double_Pipe()
+  {
+    var parts = new CodedParts();
+    var sizes = new List<PartSize>() { PartSize.SIZE_075, PartSize.SIZE_100, PartSize.SIZE_125 };
+
+    foreach (var pipeSize in sizes)
     {
-      var parts = new CodedParts();
-      var sizes = new List<PartSize>() { PartSize.SIZE_075, PartSize.SIZE_100, PartSize.SIZE_125 };
+      parts.Clear();
+      DripAdapter.AddPOLYFittings(parts, pipeSize, pipeSize);
 
-      foreach (var pipeSize in sizes)
-      {
-        parts.Clear();
-        DripAdapter.AddPOLYFittings(parts, pipeSize, pipeSize);
-
-        Assert.That(parts.Count, Is.EqualTo(4));
-        Assert.That(parts[$"PF{pipeSize}HDSADDLE"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"FPSBE050"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"FUNNYPIPE"].Quantity, Is.EqualTo(1));
-        Assert.That(parts[$"HRFIG8"].Quantity, Is.EqualTo(1));
-      }
+      Assert.That(parts.Count, Is.EqualTo(4));
+      Assert.That(parts[$"PF{pipeSize}HDSADDLE"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"FPSBE050"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"FUNNYPIPE"].Quantity, Is.EqualTo(1));
+      Assert.That(parts[$"HRFIG8"].Quantity, Is.EqualTo(1));
     }
+  }
+
+  [Test]
+  public void Constructor_Tests()
+  {
+    var sut = new DripAdapter();
+    Assert.That(sut.Stub, Is.EqualTo(StubType.PVC));
+
+    sut.Stub = StubType.FunnyPipe;
+    Assert.That(sut.Stub, Is.EqualTo(StubType.FunnyPipe));
+
+    var newSut = new DripAdapter(sut);
+    Assert.That(newSut.Stub, Is.EqualTo(StubType.FunnyPipe));
+
+    var clone = newSut.Clone() as DripAdapter;
+    Assert.That(clone, Is.Not.Null);
+    Assert.That(clone.Stub, Is.EqualTo(StubType.FunnyPipe));
   }
 }
