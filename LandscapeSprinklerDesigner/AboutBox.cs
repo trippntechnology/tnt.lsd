82b6bfda9cff8e70a3bd62582c47551e90ffe311
Steve Tripp <steve@trippntechnology.com>
2025-05-14 10:34:23 -0600

    Refactor AboutBox and enhance ListView sorting
    Significantly updated the AboutBox UI layout in Designer.cs, correcting indentation and adjusting control properties. Introduced a new ListViewColumnSorter in AboutBox.cs for improved sorting functionality, along with refactored assembly attribute accessors.
    
    Updated PartsListForm.cs for consistent formatting and improved column sorting logic.

--- LandscapeSprinklerDesigner/AboutBox.cs
+++ LandscapeSprinklerDesigner/AboutBox.cs
@@ -1,59 +1,86 @@
-﻿using System.Reflection;
+﻿using LandscapeSprinklerDesigner.Comparer;
+using System.Reflection;
 using TNT.Commons;
-using TNT.Utilities;
 
-namespace LandscapeSprinklerDesigner
+namespace LandscapeSprinklerDesigner;
+
+partial class AboutBox : Form
 {
-  partial class AboutBox : Form
+  // ListView column sorter
+  private readonly ListViewColumnSorter lvwColumnSorter;
+
+  public AboutBox()
   {
-    public AboutBox()
+    InitializeComponent();
+
+    // Create an instance of a ListView column sorter and assign it to the ListView control
+    lvwColumnSorter = new ListViewColumnSorter();
+    listView1.ListViewItemSorter = lvwColumnSorter;
+
+    this.Text = String.Format("About {0}", AssemblyTitle);
+    this.VersionLabel.Text = String.Format("Version {0}", AssemblyVersion);
+    this.CopyrightLabel.Text = AssemblyCopyright;
+  }
+
+  #region Assembly Attribute Accessors
+
+  private readonly Assembly _executingAssembly = Assembly.GetExecutingAssembly();
+
+  public string AssemblyTitle => _executingAssembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title ?? string.Empty;
+  public string AssemblyVersion => Global.getVersion();
+  public string AssemblyDescription => _executingAssembly.GetCustomAttribute<AssemblyDescriptionAttribute>()?.Description ?? string.Empty;
+  public string AssemblyProduct => _executingAssembly.GetCustomAttribute<AssemblyProductAttribute>()?.Product ?? string.Empty;
+  public string AssemblyCopyright => _executingAssembly.GetCustomAttribute<AssemblyCopyrightAttribute>()?.Copyright ?? string.Empty;
+  public string AssemblyCompany => _executingAssembly.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? string.Empty;
+
+  #endregion
+
+  private void AboutBox_Load(object sender, EventArgs e)
+  {
+    string? executablePath = Path.GetDirectoryName(Application.ExecutablePath);
+    List<string> files = Path.GetDirectoryName(Application.ExecutablePath)?.Let(path =>
     {
-      InitializeComponent();
-      this.Text = String.Format("About {0}", AssemblyTitle);
-      this.VersionLabel.Text = String.Format("Version {0}", AssemblyVersion);
-      this.CopyrightLabel.Text = AssemblyCopyright;
-    }
+      var list = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories).Distinct().Order().ToList();
+      return list.DistinctBy(x => Path.GetFileName(x)).ToList();
+    }) ?? new List<string>();
 
-    #region Assembly Attribute Accessors
-
-    public string AssemblyTitle { get { return Utilities.GetAssemblyAttribute<AssemblyTitleAttribute>(Assembly.GetExecutingAssembly())?.Title ?? string.Empty; } }
-    public string AssemblyVersion => Global.getVersion();
-    public string AssemblyDescription { get { return Utilities.GetAssemblyAttribute<AssemblyDescriptionAttribute>(Assembly.GetExecutingAssembly())?.Description ?? string.Empty; } }
-    public string AssemblyProduct { get { return Utilities.GetAssemblyAttribute<AssemblyProductAttribute>(Assembly.GetExecutingAssembly())?.Product ?? string.Empty; } }
-    public string AssemblyCopyright { get { return Utilities.GetAssemblyAttribute<AssemblyCopyrightAttribute>(Assembly.GetExecutingAssembly())?.Copyright ?? string.Empty; } }
-    public string AssemblyCompany { get { return Utilities.GetAssemblyAttribute<AssemblyCompanyAttribute>(Assembly.GetExecutingAssembly())?.Company ?? string.Empty; } }
-
-    #endregion
-
-    private void AboutBox_Load(object sender, EventArgs e)
+    foreach (string file in files)
     {
-      string? executablePath = Path.GetDirectoryName(Application.ExecutablePath);
-      List<string> files = Path.GetDirectoryName(Application.ExecutablePath)?.let(path =>
+      try
       {
-        return Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories).Order().ToList();
-      }) ?? new List<string>();
+        Assembly asm = Assembly.LoadFile(file);
+        ListViewItem item = listView1.Items.Add(Path.GetFileName(file)); item.SubItems.Add(asm.GetName().Version?.ToString() ?? string.Empty);
 
-      foreach (string file in files)
-      {
-        try
-        {
-          Assembly asm = Assembly.LoadFile(file);
-          ListViewItem item = listView1.Items.Add(Path.GetFileName(file));
-
-          item.SubItems.Add(asm.GetName().Version?.ToString() ?? string.Empty);
-
-          AssemblyCopyrightAttribute? assCopyAttr = Utilities.GetAssemblyAttribute<AssemblyCopyrightAttribute>(asm);
-          if (assCopyAttr != null)
-          {
-            item.SubItems.Add(assCopyAttr.Copyright);
-          }
-          else
-          {
-            item.SubItems.Add(string.Empty);
-          }
-        }
-        catch { }
+        AssemblyCopyrightAttribute? assCopyAttr = asm.GetCustomAttribute<AssemblyCopyrightAttribute>();
+        item.SubItems.Add(assCopyAttr?.Copyright ?? string.Empty);
       }
+      catch { }
     }
+  }
+
+  /// <summary>
+  /// Handles the ColumnClick event of the ListView control.
+  /// </summary>
+  /// <param name="sender">The source of the event.</param>
+  /// <param name="e">The event data.</param>
+  private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+  {
+    // Determine if the clicked column is already the column that is being sorted
+    if (e.Column == lvwColumnSorter.SortColumn)
+    {
+      // Reverse the current sort direction for this column
+      lvwColumnSorter.Order = lvwColumnSorter.Order == SortOrder.Ascending
+          ? SortOrder.Descending
+          : SortOrder.Ascending;
+    }
+    else
+    {
+      // Set the column number that is to be sorted; default to ascending
+      lvwColumnSorter.SortColumn = e.Column;
+      lvwColumnSorter.Order = SortOrder.Ascending;
+    }
+
+    // Perform the sort with these new sort options
+    listView1.Sort();
   }
 }
--- LandscapeSprinklerDesigner/Comparer/ListViewColumnSorter.cs
+++ LandscapeSprinklerDesigner/Comparer/ListViewColumnSorter.cs
@@ -1,61 +1,110 @@
-﻿using System.Collections;
+using System.Collections;
 
 namespace LandscapeSprinklerDesigner.Comparer;
 
+/// <summary>
+/// This class is an implementation of the 'IComparer' interface and provides
+/// sorting functionality for ListView columns.
+/// </summary>
 public class ListViewColumnSorter : IComparer
 {
-  public int Column { get; set; }
-  public SortOrder Order { get; set; }
-  public CaseInsensitiveComparer Comparer { get; set; }
+  /// <summary>
+  /// Specifies the column to be sorted
+  /// </summary>
+  private int ColumnToSort;
 
+  /// <summary>
+  /// Specifies the order in which to sort (i.e. 'Ascending' or 'Descending')
+  /// </summary>
+  private SortOrder OrderOfSort;
+
+  /// <summary>
+  /// Case insensitive comparer object
+  /// </summary>
+  private readonly CaseInsensitiveComparer ObjectCompare;
+
+  /// <summary>
+  /// Class constructor. Initializes various elements
+  /// </summary>
   public ListViewColumnSorter()
   {
-    Column = 0;
-    Order = SortOrder.None;
-    Comparer = new CaseInsensitiveComparer();
+    // Initialize the column to '0'
+    ColumnToSort = 0;
+
+    // Initialize the sort order to 'none'
+    OrderOfSort = SortOrder.None;
+
+    // Initialize the CaseInsensitiveComparer object
+    ObjectCompare = new CaseInsensitiveComparer();
   }
 
-  public int Compare(object x, object y)
+  /// <summary>
+  /// This method is inherited from the IComparer interface. It compares the two objects passed using a case insensitive comparison.
+  /// </summary>
+  /// <param name="x">First object to be compared</param>
+  /// <param name="y">Second object to be compared</param>
+  /// <returns>The result of the comparison. "0" if equal, negative if 'x' is less than 'y' and positive if 'x' is greater than 'y'</returns>
+  public int Compare(object? x, object? y)
   {
-    int compareResult;
-    ListViewItem listviewX, listviewY;
+    if (x == null || y == null)
+      return 0;
 
     // Cast the objects to be compared to ListViewItem objects
-    listviewX = (ListViewItem)x;
-    listviewY = (ListViewItem)y;
+    ListViewItem listviewX = (ListViewItem)x;
+    ListViewItem listviewY = (ListViewItem)y;
 
-    var subItemsX = listviewX.SubItems;
-    var subItemsY = listviewY.SubItems;
+    // Get the sub-item values for the column being sorted
+    string stringX = listviewX.SubItems.Count > ColumnToSort
+        ? listviewX.SubItems[ColumnToSort].Text
+        : string.Empty;
+    string stringY = listviewY.SubItems.Count > ColumnToSort
+        ? listviewY.SubItems[ColumnToSort].Text
+        : string.Empty;
+
+    // Determine if the column contains numeric or version data
+    if (ColumnToSort == 1) // Version column
+    {
+      // Try to parse as version
+      if (Version.TryParse(stringX, out var versionX) && Version.TryParse(stringY, out var versionY))
+        return OrderOfSort == SortOrder.Ascending ? versionX.CompareTo(versionY) : versionY.CompareTo(versionX);
+    }
 
     // Compare the two items
-    if (Column == 2)
-    {
-      compareResult = Convert.ToInt32(listviewX.SubItems[Column].Text) - Convert.ToInt32(listviewY.SubItems[Column].Text);
-    }
-    else if (Column < subItemsX.Count && Column < subItemsY.Count)
-    {
-      compareResult = Comparer.Compare(listviewX.SubItems[Column].Text, listviewY.SubItems[Column].Text);
-    }
-    else
-    {
-      compareResult = 1;
-    }
+    int compareResult = ObjectCompare.Compare(stringX, stringY);
 
     // Calculate correct return value based on object comparison
-    if (Order == SortOrder.Ascending)
+    if (OrderOfSort == SortOrder.Ascending)
     {
       // Ascending sort is selected, return normal result of compare operation
       return compareResult;
     }
-    else if (Order == SortOrder.Descending)
+    else if (OrderOfSort == SortOrder.Descending)
     {
       // Descending sort is selected, return negative result of compare operation
-      return (-compareResult);
+      return -compareResult;
     }
     else
     {
       // Return '0' to indicate they are equal
       return 0;
     }
+  }
+
+  /// <summary>
+  /// Gets or sets the number of the column to which to apply the sorting operation (Defaults to '0').
+  /// </summary>
+  public int SortColumn
+  {
+    set => ColumnToSort = value;
+    get => ColumnToSort;
+  }
+
+  /// <summary>
+  /// Gets or sets the order of sorting to apply (for example, 'Ascending' or 'Descending').
+  /// </summary>
+  public SortOrder Order
+  {
+    set => OrderOfSort = value;
+    get => OrderOfSort;
   }
 }
