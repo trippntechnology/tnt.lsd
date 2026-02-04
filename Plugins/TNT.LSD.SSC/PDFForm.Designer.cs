395a747af703ab728324e4216602cdd3b37348ce
stevetripp <stripp@trippntechnology.com>
2026-02-04 08:30:54 -0700

    Replace WebBrowser with WebView2 for PDF viewing
    PDFForm now uses Microsoft Edge WebView2 instead of the legacy WebBrowser control for displaying PDFs, improving compatibility and rendering. Added runtime checks for WebView2 installation and updated project files to include the Microsoft.Web.WebView2 NuGet package. Also updated TNT.Plugin.Manager to v2.0.3 and made minor resource and formatting adjustments.

--- Plugins/TNT.LSD.SSC/PDFForm.Designer.cs
+++ Plugins/TNT.LSD.SSC/PDFForm.Designer.cs
@@ -20,42 +20,49 @@ partial class PDFForm
     base.Dispose(disposing);
   }
 
-  #region Windows Form Designer generated code
+    #region Windows Form Designer generated code
 
-  /// <summary>
-  /// Required method for Designer support - do not modify
-  /// the contents of this method with the code editor.
-  /// </summary>
-  private void InitializeComponent()
-  {
-    System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PDFForm));
-    this.Browser = new System.Windows.Forms.WebBrowser();
-    this.SuspendLayout();
-    // 
-    // Browser
-    // 
-    this.Browser.Dock = System.Windows.Forms.DockStyle.Fill;
-    this.Browser.Location = new System.Drawing.Point(0, 0);
-    this.Browser.MinimumSize = new System.Drawing.Size(20, 20);
-    this.Browser.Name = "Browser";
-    this.Browser.Size = new System.Drawing.Size(284, 262);
-    this.Browser.TabIndex = 0;
-    // 
-    // PDFForm
-    // 
-    this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-    this.ClientSize = new System.Drawing.Size(284, 262);
-    this.Controls.Add(this.Browser);
-    this.DockAreas = WeifenLuo.WinFormsUI.Docking.DockAreas.Document;
-    this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-    this.Name = "PDFForm";
-    this.Text = "PDFForm";
-    this.ResumeLayout(false);
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PDFForm));
+        Browser = new Microsoft.Web.WebView2.WinForms.WebView2();
+        ((System.ComponentModel.ISupportInitialize)Browser).BeginInit();
+        SuspendLayout();
+        // 
+        // Browser
+        // 
+        Browser.AllowExternalDrop = true;
+        Browser.CreationProperties = null;
+        Browser.DefaultBackgroundColor = System.Drawing.Color.White;
+        Browser.Dock = System.Windows.Forms.DockStyle.Fill;
+        Browser.Location = new System.Drawing.Point(0, 0);
+        Browser.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        Browser.Name = "Browser";
+        Browser.Size = new System.Drawing.Size(911, 787);
+        Browser.TabIndex = 0;
+        Browser.ZoomFactor = 1D;
+        // 
+        // PDFForm
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(911, 787);
+        Controls.Add(Browser);
+        DockAreas = WeifenLuo.WinFormsUI.Docking.DockAreas.Document;
+        Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+        Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+        Name = "PDFForm";
+        Text = "PDFForm";
+        ((System.ComponentModel.ISupportInitialize)Browser).EndInit();
+        ResumeLayout(false);
 
-  }
+    }
 
-  #endregion
+    #endregion
 
-  private System.Windows.Forms.WebBrowser Browser;
+    private Microsoft.Web.WebView2.WinForms.WebView2 Browser;
 }
--- Plugins/TNT.LSD.SSC/PDFForm.cs
+++ Plugins/TNT.LSD.SSC/PDFForm.cs
@@ -1,18 +1,46 @@
-﻿using WeifenLuo.WinFormsUI.Docking;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
 
 namespace TNT.LSD.SSC;
 
 public partial class PDFForm : DockContent
 {
-  public PDFForm()
-  {
-    InitializeComponent();
-  }
+    public PDFForm()
+    {
+        InitializeComponent();
+    }
 
-  public void Show(string fileName, DockPanel dockPanel, DockState dockState)
-  {
-    Text = fileName;
-    Browser.Navigate(string.Concat(@"file:\", fileName));
-    Show(dockPanel, dockState);
-  }
+    public async void Show(string fileName, DockPanel dockPanel, DockState dockState)
+    {
+        if (!IsWebView2Installed())
+        {
+            MessageBox.Show("Microsoft Edge WebView2 Runtime is not installed. Please install it to view PDF files.", "WebView2 Not Installed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        Text = fileName;
+        await Browser.EnsureCoreWebView2Async();
+        Browser.Source = new Uri(fileName);
+        Show(dockPanel, dockState);
+    }
+
+    public bool IsWebView2Installed()
+    {
+        try
+        {
+            // This returns the version string (e.g., "121.0.2277.83") if found.
+            // It throws an exception if the runtime is missing.
+            string version = CoreWebView2Environment.GetAvailableBrowserVersionString();
+            return !string.IsNullOrEmpty(version);
+        }
+        catch (WebView2RuntimeNotFoundException)
+        {
+            return false;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+    }
 }
